12de9a490d54f1fd37efbc68f3a53925378e7aa0
andrew9488 <pashkevichandrew@gmail.com>
2021-05-08 09:55:18 +0300

    feat: create store and employees-reducer

---
+++ task-6/my-app/src/bll/employees-reducer.ts
@@ -0,0 +1,81 @@
+import {employeesAPI} from "../dal/api";
+import {AppRootStateType} from "./store";
+import {ThunkAction, ThunkDispatch} from "redux-thunk";
+
+type ActionsType =
+    ReturnType<typeof getEmployeesAC>
+    | ReturnType<typeof removeEmployeeAC>
+    | ReturnType<typeof addEmployeeAC>
+
+type ThunkType = ThunkAction<void, AppRootStateType, unknown, ActionsType>
+
+export type EmployeeType = {
+    id: number,
+    email: string,
+    first_name: string
+    last_name: string
+    avatar: string
+}
+
+type SupportType = {
+    url: string
+    text: string
+}
+
+export type EmployeesType = {
+    page: number
+    per_page: number
+    total: number
+    total_pages: number
+    data: Array<EmployeeType>
+    support: SupportType
+}
+
+let initialState = {} as EmployeesType
+
+export const employeesReducer = (state: EmployeesType = initialState, action: ActionsType): EmployeesType => {
+    switch (action.type) {
+        case "EMPLOYEES-REDUCER/GET-EMPLOYEES":
+            return {
+                ...state,
+                data: action.employees
+            }
+        case "EMPLOYEES-REDUCER/REMOVE-EMPLOYEE":
+            return {
+                ...state,
+                data: state.data.filter(e => e.id !== action.id)
+            }
+        case "EMPLOYEES-REDUCER/ADD-EMPLOYEE":
+            let newEmployee: EmployeeType = {
+                id: new Date().getTime(),
+                first_name: action.firstName,
+                last_name: action.lastName,
+                avatar: "avatar",
+                email: "newEmployee@gmail.com"
+            }
+            return {
+                ...state,
+                data: [newEmployee, ...state.data]
+            }
+        default:
+            return state
+    }
+}
+
+const getEmployeesAC = (employees: Array<EmployeeType>) =>
+    ({type: "EMPLOYEES-REDUCER/GET-EMPLOYEES", employees} as const)
+
+export const removeEmployeeAC = (id: number) =>
+    ({type: "EMPLOYEES-REDUCER/REMOVE-EMPLOYEE", id} as const)
+
+export const addEmployeeAC = (firstName: string, lastName: string) =>
+    ({type: "EMPLOYEES-REDUCER/ADD-EMPLOYEE", firstName, lastName} as const)
+
+export const getEmployeesTC = (): ThunkType => {
+    return (dispatch: ThunkDispatch<AppRootStateType, unknown, ActionsType>) => {
+        return employeesAPI.getEmployees()
+            .then(res => {
+                dispatch(getEmployeesAC(res.data))
+            })
+    }
+}
---
+++ task-6/my-app/src/bll/store.ts
@@ -0,0 +1,12 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {employeesReducer} from "./employees-reducer";
+import thunk from "redux-thunk";
+
+
+const rootReducer = combineReducers({
+    employees: employeesReducer
+})
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
